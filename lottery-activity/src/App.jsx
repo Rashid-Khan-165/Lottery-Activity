38a1b46ad943fa2ecebcad138dae4773c2e0dd13
Rashid Khan <williamsonkane7539@gmail.com>
2024-09-18 14:53:19 +0530

    seprated all component with each other

--- lottery-activity/src/App.jsx
+++ lottery-activity/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Lottery from './Lottery.jsx'
 
+
 function App() {
+  let winCondition = (ticket) => {
+      return ticket[0] === 0;
+  }
   return (
     <>
-      <Lottery/>
+      <Lottery n={3} winCondition = {winCondition}/>
     </>
   )
 }
--- lottery-activity/src/Lottery.jsx
+++ lottery-activity/src/Lottery.jsx
@@ -1,23 +1,20 @@
 import { useState } from "react"
 import "./Lottery.css";
 import { genTicket, sum } from "./helper";
+import Ticket from "./Ticket";
 
-export default function lottery(){
+export default function lottery({n = 3, winCondition }){
     let [ticket , setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) === 15;
+    let isWinning = winCondition(ticket);
 
     let buyTicket = () =>{
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     }
 
     return(
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
+            <Ticket ticket={ticket}/>
             <br/> 
             <button onClick={buyTicket}>Buy New Ticket</button>
             <h3>{isWinning && "Congratulations, you won!"}</h3>
